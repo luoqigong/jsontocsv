c176036dd4a594778ea827e1c647b8cdd8210664
luoqigong <dreamcatcherluo@163.com>
2026-05-06 19:43:38 +0800

    fix(types): JsonArray type alias for eslint compat

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export type JsonValue = string | number | boolean | null | JsonObject | JsonArray
 export interface JsonObject { [key: string]: JsonValue }
-export interface JsonArray extends Array<JsonValue> {}
+export type JsonArray = Array<JsonValue>
 
 export type NestingStrategy = 'flatten' | 'stringify' | 'explode' | 'multiSheet'
 
